513b0e8ad0ad56fe4a437036b6a44802eace7670
Enmanuel Jarquín <enmisac@gmail.com>
2023-11-16 22:29:26 -0600

    refactor(backend): replace http request library

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,21 +1,36 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import fetch from 'node-fetch';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   async getCommits() {
-    const res = await fetch(
-      'https://api.github.com/repos/TheEnmanuel23/fulltime-force-challenge/commits',
-      {
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${process.env.GT_TOKEN}`,
-          'X-GitHub-Api-Version': '2022-11-28',
-        },
-      },
+    const token = process.env.GT_TOKEN;
+    const ghUsername = process.env.GT_USERNAME;
+    const ghRepository = process.env.GT_REPOSITORY;
+    const res = await firstValueFrom(
+      this.httpService
+        .get(
+          `https://api.github.com/repos/${ghUsername}/${ghRepository}/commits`,
+          {
+            headers: {
+              Accept: 'application/vnd.github+json',
+              Authorization: `Bearer ${token}`,
+              'X-GitHub-Api-Version': '2022-11-28',
+            },
+          },
+        )
+        .pipe(
+          catchError((error: AxiosError) => {
+            console.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
     );
 
-    const data = await res.json();
-    return data;
+    return res.data;
   }
 }
